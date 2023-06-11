0ce854565edabf84edf2cac5f813771174ca2394
abdulbosittojibayev <abdulbositsmall@gmail.com>
2023-06-11 14:55:39 -0700

    salom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import Navbar from "./components/Navbar/Navbar.js";
 
@@ -7,18 +6,6 @@ function App() {
 		<div className='App'>
 			<Navbar/>
 		</div>
-	);
-=======
-import React from 'react'
-import Footer from './components/Footer/Footer'
-
-const App = () => {
-  return (
-    <div className='app'>
-      <Footer />
-    </div>
-  )
->>>>>>> bf74b71cf3aa214be04ca94cfc6ac36b3fef66df
-}
+	)};
 
 export default App;
