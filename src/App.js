d0cb4159d07d4729f465461a9e71d2a4131ee2f3
Sardordaminov <sardorbe221007@gmail.com>
2023-06-12 17:48:50 +0500

    root deleted

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import Navbar from "./components/Navbar/Navbar.js";
 import Footer from './components/Footer/Footer.js';
+import AllBlogs from './components/AllBlogs/AllBlogs.js';
 
 function App() {
+	const allblogs = [
+		{
+			id: 1,
+			title: 'Мужские Кроссовки Nike Blazer Mid Suede',
+			img: 'sneaker.jpg',
+			price: '12999',
+		},
+	]
 	return (
 		<div className='App'>
-			<Navbar/>
-      <Footer />
+			<Navbar />
+			<AllBlogs allblogs={allblogs}/>
+			<Footer />
 		</div>
-	)};
+	)
+};
 
 export default App;
