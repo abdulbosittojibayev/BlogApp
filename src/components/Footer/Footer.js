e61cb0ff82bc3669a9565d45a1fa4ad04b7e2216
Sardordaminov <sardorbe221007@gmail.com>
2023-06-12 14:24:59 +0500

    salom

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,17 @@
+
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+        <div>
+            <p>© 2023</p>
+            <p>Twitter</p>
+            <p>LinkedIn</p>
+            <p>Email</p>
+            <p>RSS feed</p>
+            <p>Add to Feedly</p>
+        </div>
+    </div>
+  )
+}
