ab183e053c52cf0452ea1298d71ea8efc16432bd
Sardordaminov <sardorbe221007@gmail.com>
2023-06-12 15:04:45 +0500

    salom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from "./components/Navbar/Navbar.js";
+import Footer from './components/Footer/Footer.js';
 
 function App() {
 	return (
 		<div className='App'>
 			<Navbar/>
+      <Footer />
 		</div>
 	)};
 
